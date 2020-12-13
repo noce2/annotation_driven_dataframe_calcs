1d346a3d4192f3bc561d2b7f0fc9e608f9a2d796
noce2 <20339399+noce2@users.noreply.github.com>
2020-12-13 08:44:34 +0000

    Use timestap constant var for field name

--- annotation_driven_dataframe_calcs/annotation_driven_dataframe_calcs.py
+++ annotation_driven_dataframe_calcs/annotation_driven_dataframe_calcs.py
@@ -3,6 +3,8 @@ from typing import Union
 import pandas
 import dask.dataframe as dd
 
+from annotation_driven_dataframe_calcs.column_names import TIMESTEP_NO
+
 def run(
     input_data, run_config
 ):
@@ -12,12 +14,12 @@ def expand_for_timesteps(
     input_data, first_time_step: int, last_time_step: int
 ):
     interim_frame = input_data
-    interim_frame["TIMESTEP_NO"] = [
+    interim_frame[TIMESTEP_NO] = [
         list(
             range(first_time_step, last_time_step+1)
         )
     ] * len(input_data.index)
-    interim_frame_exploded = interim_frame.explode(column="TIMESTEP_NO", ignore_index=True)
+    interim_frame_exploded = interim_frame.explode(column=TIMESTEP_NO, ignore_index=True)
 
 
     return interim_frame_exploded
