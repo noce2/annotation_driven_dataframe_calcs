1920e1649794dd66f2b98ce9cf78df7d71716de2
noce2 <20339399+noce2@users.noreply.github.com>
2020-12-14 10:32:42 +0000

    Add LRU cache utility

---
+++ annotation_driven_dataframe_calcs/caching_tools.py
@@ -0,0 +1,24 @@
+from typing import OrderedDict
+from loguru import logger
+
+
+class LRU(OrderedDict):
+    'Limit size, evicting the least recently looked-up key when full'
+
+    def __init__(self, maxsize=128, /, *args, **kwds):
+        self.maxsize = maxsize
+        super().__init__(*args, **kwds)
+
+    def __getitem__(self, key):
+        logger.debug(f"searching for key {key} in the cache")
+        value = super().__getitem__(key)
+        self.move_to_end(key)
+        return value
+
+    def __setitem__(self, key, value):
+        if key in self:
+            self.move_to_end(key)
+        super().__setitem__(key, value)
+        if len(self) > self.maxsize:
+            oldest = next(iter(self))
+            del self[oldest]
