91b4b1c1723837c3124d8fad449464e1b8370079
noce2 <20339399+noce2@users.noreply.github.com>
2020-12-14 19:46:54 +0000

    Fix first stage calc creator

--- annotation_driven_dataframe_calcs/first_stage_calcs.py
+++ annotation_driven_dataframe_calcs/first_stage_calcs.py
@@ -1,6 +1,13 @@
+from typing import Tuple
+from loguru import logger
+
+from numpy.core.numeric import roll
 from annotation_driven_dataframe_calcs.caching_tools import LRU
 from annotation_driven_dataframe_calcs.column_names import (
-    ACCOUNT_NO, PARAM_A,
+    ACCOUNT_NO,
+    PARAM_A,
+    PARAM_B,
+    PARAM_C,
     SERIES_A_PRIME,
     TIMESTEP_NO,
 )
@@ -14,19 +21,68 @@ def generate_series_a_prime(
         input_data_set_for_timesteps.groupby([ACCOUNT_NO, TIMESTEP_NO])[PARAM_A]
         .rolling(window=2, min_periods=1)
         .apply(
-            cache_enabled_generate_series_a_prime_mapper_generator(9),
+            cache_enabled_generate_series_a_prime_mapper_generator(
+                cache_size_limit=9, tuple_of_initial_values=(100,)
+            ),
             kwargs={"entire_input_data_set": input_data_set_for_timesteps},
         )
     )
     return series_to_return.rename(SERIES_A_PRIME)
 
 
-def cache_enabled_generate_series_a_prime_mapper_generator(cache_size_limit):
+def cache_enabled_generate_series_a_prime_mapper_generator(
+    cache_size_limit: int, tuple_of_initial_values: Tuple
+):
     cache = LRU(maxsize=cache_size_limit)
 
+    if type(tuple_of_initial_values) != tuple:
+        raise TypeError(
+            f"the argument 'tuple_of_initial_values' should be a tuple"
+            f"but a {type(tuple_of_initial_values)} was found instead."
+        )
+
     def closed_generate_series_a_prime_mapper(
-        x, entire_input_data_set: pandas.DataFrame
+        rolling_window_series: pandas.Series, entire_input_data_set: pandas.DataFrame
     ):
-        return 1
+        account_no, timestep_no = rolling_window_series.tail(n=1).index[0]
+        logger.debug(
+            f"rolling window is currently at {account_no} time_step {timestep_no}"
+        )
+
+        if 0 < timestep_no <= len(tuple_of_initial_values):
+            cache[f"{account_no}_{timestep_no}"] = tuple_of_initial_values[
+                timestep_no - 1
+            ]
+        else:
+            list_of_previous_computations = []
+
+            for i in range(len(tuple_of_initial_values), 0, -1):
+                list_of_previous_computations.append(cache[f"{account_no}_{timestep_no-i}"])
+
+            previous_values = tuple(list_of_previous_computations)
+
+            cache[f"{account_no}_{timestep_no}"] = core_arithmetic_for_current_step(
+                account_no=account_no,
+                timestep_no=timestep_no,
+                current_window_series=rolling_window_series,
+                tuple_of_previous_calculated_values=previous_values,
+                entire_input_data_set=entire_input_data_set,
+            )
+
+        return cache[f"{account_no}_{timestep_no}"]
 
     return closed_generate_series_a_prime_mapper
+
+
+def core_arithmetic_for_current_step(
+    account_no,
+    timestep_no,
+    current_window_series,
+    tuple_of_previous_calculated_values,
+    entire_input_data_set,
+) -> float:
+    return (
+        tuple_of_previous_calculated_values[0]
+        - entire_input_data_set.loc[(account_no, timestep_no), PARAM_B]
+        - entire_input_data_set.loc[(account_no, timestep_no), PARAM_C]
+    )
