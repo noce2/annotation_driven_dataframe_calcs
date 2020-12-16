a107d201f26f77f4fe02d0ffabe5c0cdb16c4a73
noce2 <20339399+noce2@users.noreply.github.com>
2020-12-16 01:59:13 +0000

    Add annotation for recursive calcs

--- annotation_driven_dataframe_calcs/calculation_helpers.py
+++ annotation_driven_dataframe_calcs/calculation_helpers.py
@@ -1,7 +1,11 @@
+from annotation_driven_dataframe_calcs.caching_tools import LRU
+from typing import Tuple
 from loguru import logger
 import pandas
 from annotation_driven_dataframe_calcs.column_names import ACCOUNT_NO, TIMESTEP_NO
 from functools import wraps
+
+CACHE_BUFFER_FACTOR_FOR_RECURSIVE_CALCS = 4
 
 
 def calculate_over_window(
@@ -40,3 +44,51 @@ def adapt_stepwise_nonrecursive_calc_for_window(func):
                 entire_input_data_set=entire_input_data_set,
             )
     return window_adapted_calc
+
+def recursive_calculation(
+    number_of_previous_terms_needed: int,
+    tuple_of_initial_values: Tuple
+):
+    if not(len(tuple_of_initial_values) == number_of_previous_terms_needed):
+        error_message=(
+            f"The number of previous terms needed,{number_of_previous_terms_needed},"
+            f" must match the number of initial values for the calculation, {tuple_of_initial_values}"
+        )
+        raise ValueError(error_message)
+
+    def decorate_recursive_calculation(core_arithmetic_for_current_step):
+        @wraps(core_arithmetic_for_current_step)
+        def wrap_recursive_calculation(
+            rolling_window_series: pandas.Series, entire_input_data_set: pandas.DataFrame
+        ):
+            account_no, timestep_no = rolling_window_series.tail(n=1).index[0]
+            logger.debug(
+                f"rolling window is currently at {account_no} time_step {timestep_no}"
+            )
+
+            if 0 < timestep_no <= len(tuple_of_initial_values):
+                wrap_recursive_calculation.cache[f"{account_no}_{timestep_no}"] = tuple_of_initial_values[
+                    timestep_no - 1
+                ]
+            else:
+                list_of_previous_computations = []
+
+                for i in range(len(tuple_of_initial_values), 0, -1):
+                    list_of_previous_computations.append(
+                        wrap_recursive_calculation.cache[f"{account_no}_{timestep_no-i}"]
+                    )
+
+                previous_values = tuple(list_of_previous_computations)
+
+                wrap_recursive_calculation.cache[f"{account_no}_{timestep_no}"] = core_arithmetic_for_current_step(
+                    account_no=account_no,
+                    timestep_no=timestep_no,
+                    current_window_series=rolling_window_series,
+                    tuple_of_previous_calculated_values=previous_values,
+                    entire_input_data_set=entire_input_data_set,
+                )
+
+            return wrap_recursive_calculation.cache[f"{account_no}_{timestep_no}"]
+        wrap_recursive_calculation.cache = LRU(maxsize=number_of_previous_terms_needed*CACHE_BUFFER_FACTOR_FOR_RECURSIVE_CALCS)
+        return wrap_recursive_calculation
+    return decorate_recursive_calculation
--- annotation_driven_dataframe_calcs/first_stage_calcs.py
+++ annotation_driven_dataframe_calcs/first_stage_calcs.py
@@ -12,7 +12,7 @@ from annotation_driven_dataframe_calcs.column_names import (
     TIMESTEP_NO,
 )
 from annotation_driven_dataframe_calcs.registry import register
-from annotation_driven_dataframe_calcs.calculation_helpers import adapt_stepwise_nonrecursive_calc_for_window, calculate_over_window
+from annotation_driven_dataframe_calcs.calculation_helpers import adapt_stepwise_nonrecursive_calc_for_window, calculate_over_window, recursive_calculation
 
 
 @register(output_series_name=SERIES_A_PRIME)
@@ -105,6 +105,29 @@ def new_core_arithmetric_for_series_b_prime(
     entire_input_data_set,
 ) -> float:
     return (
+        - entire_input_data_set.loc[(account_no, timestep_no), PARAM_B]
+        - entire_input_data_set.loc[(account_no, timestep_no), PARAM_C]
+    )
+
+@register(output_series_name=f"{SERIES_A_PRIME}_FROM_DECS")
+@calculate_over_window(
+    series_to_run_window_over=PARAM_A,
+    window_size=2,
+    output_series_name=f"{SERIES_A_PRIME}_FROM_DECS"
+)
+@recursive_calculation(
+    number_of_previous_terms_needed=1,
+    tuple_of_initial_values=(100,)
+)
+def series_c_prime(
+    account_no,
+    timestep_no,
+    current_window_series,
+    tuple_of_previous_calculated_values,
+    entire_input_data_set,
+) -> float:
+    return (
+        tuple_of_previous_calculated_values[0]
         - entire_input_data_set.loc[(account_no, timestep_no), PARAM_B]
         - entire_input_data_set.loc[(account_no, timestep_no), PARAM_C]
     )
